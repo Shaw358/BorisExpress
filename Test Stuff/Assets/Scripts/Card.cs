9749537777f0d41d8c830d356526ab5d8a989fa4
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-12-27 19:04:30 +0100

    Poggers

---
+++ Test Stuff/Assets/Scripts/Card.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Card : MonoBehaviour
+{
+    public float value;
+
+    public List<Card> _cards = new List<Card>();
+    public List<LineRenderer> _line = new List<LineRenderer>();
+
+    public TextMeshPro _text;
+
+    private void Awake()
+    {
+        _text.text = value.ToString();
+        for(int i = 0; i < _cards.Count; i++)
+        {
+            _line[i].SetPosition(0, transform.position);
+            _line[i].SetPosition(1, _cards[i].gameObject.transform.position);
+        }
+    }
+
+    private void Update()
+    {
+        _text.text = value.ToString();
+    }
+
+    public void StartDividing()
+    {
+        StartCoroutine(Divide());
+    }
+
+    public IEnumerator Divide()
+    {
+        for (int i = 0; i < Mathf.Infinity; i++)
+        {
+            float temp = value;
+            value = 0;
+
+            yield return new WaitForSeconds(0.01f);
+
+            for (int j = 0; j < _cards.Count; j++)
+            {
+                if(temp != 0)
+                {
+                    Debug.Log(_cards[j].gameObject.name);
+                    _cards[j].value += (temp / 3);
+                }
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
---
+++ Test Stuff/Assets/Scripts/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    int delay;
+    public List<Card> _all_cards = new List<Card>();
+
+    private void Start()
+    {
+        foreach (Card item in _all_cards)
+        {
+            item.StartDividing();
+        }
+    }
+}
